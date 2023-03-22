7c142dbd88bbd338c58505b39e853d1451fd15be
Ferry Utomo <ferry.utomo@geodan.nl>
2023-03-22 18:52:23 +0100

    Fix

--- MiniCrm.UnitTests/ZipCodeServiceUnitTests.cs
+++ MiniCrm.UnitTests/ZipCodeServiceUnitTests.cs
@@ -9,38 +9,38 @@ namespace MiniCrm.UnitTests;
 public class ZipCodeServiceUnitTests
 {
     private SendAsync? _sendAsync;
-    private ZipCodeService Sut => new ZipCodeService(_sendAsync!);
-    
+    private ZipCodeService Sut => new(_sendAsync!);
+
     [Test]
     public async Task GetZipCode_WhenSuccessful_ReturnsFromBody()
     {
         // arrange
-        var customerId = "TR-562-XJ";
+        var customerId = 12;
         var expectation = "12391";
 
         _sendAsync = _ => Task.FromResult(new HttpResponseMessage
         {
-            Content = new StringContent(expectation)
+            Content = new StringContent($"{{\"zipcode\": \"{expectation}\"}}")
         });
-        
+
         // act
         var actual = await Sut.GetZipCode(customerId);
-        
+
         // assert
         Assert.AreEqual(expectation, actual);
     }
-    
+
     [Test]
-    public async Task GetZipCode_WhenUnsuccessful_Throws()
+    public void GetZipCode_WhenUnsuccessful_Throws()
     {
         // arrange
-        var customerId = "TR-562-XJ";
+        var customerId = 31;
 
         _sendAsync = _ => Task.FromResult(new HttpResponseMessage
         {
             StatusCode = HttpStatusCode.BadRequest
         });
-        
+
         // act
         // assert
         Assert.ThrowsAsync<HttpRequestException>(() => Sut.GetZipCode(customerId));
