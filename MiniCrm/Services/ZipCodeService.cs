e6579db145d1ae3efd60bc55e05d81cae074536e
Ferry Utomo <ferry.utomo@geodan.nl>
2023-03-22 18:46:09 +0100

    Using real api

--- MiniCrm/Services/ZipCodeService.cs
+++ MiniCrm/Services/ZipCodeService.cs
@@ -1,10 +1,13 @@
-﻿namespace MiniCrm.Services;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MiniCrm.Services;
 
 public class ZipCodeService
 {
-    public async Task<string> GetZipCode(string customerId)
+    public async Task<string> GetZipCode(int customerId)
     {
-        var uri = new Uri($"http://utomo.eu/api/zipcode/{customerId}");
+        var uri = new Uri($"https://641b3a009b82ded29d4d70c3.mockapi.io/zipcode/{customerId}");
         
         var request = new HttpRequestMessage(HttpMethod.Get, uri);
 
@@ -12,9 +15,13 @@ public class ZipCodeService
 
         var response = await client.SendAsync(request);
         
-        return await response
+        var content = await response
             .EnsureSuccessStatusCode()
             .Content
             .ReadAsStringAsync();
+
+        return JsonSerializer.Deserialize<Zip>(content)!.Zipcode;
     }
+
+    private sealed record Zip([property:JsonPropertyName("zipcode")] string Zipcode);
 }
